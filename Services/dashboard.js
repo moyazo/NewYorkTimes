9a190b83ff703e2dd57cf6e7ac5f8cab752d80ec
Manuel Moya <manuel.moyavadillo2@gmail.com>
2022-12-28 17:43:57 +0100

    Finishing dashboard decoration

--- Services/dashboard.js
+++ Services/dashboard.js
@@ -27,7 +27,6 @@ return list
 
 async function startApp() {
     const booksData = await getData()
-
 booksData.forEach(result => {
    
     const ourData = {name: result.list_name, old: result.oldest_published_date,new: result.newest_published_date, update: result.updated}
@@ -43,9 +42,9 @@ booksData.forEach(result => {
     for (let i = 0; i < 3; i++) {
         const pELEM = document.createElement("p") 
         pELEM.innerHTML = i === 0 ?
-        ourData.old : i === 1 ?
-        ourData.new : i === 2 ?
-        ourData.update : "";
+        "OLDEST: " + ourData.old : i === 1 ?
+        "NEWEST: " + ourData.new : i === 2 ?
+        "UPDATED: " + ourData.update : "";
         divINFO.append(pELEM);
     }
 
@@ -71,87 +70,8 @@ startApp()
 
 // NAVBAR MENU
 
-/* <h3 class="dash">DASHBOARD</h3>
-            <h3 class="detail">DETAILS</h3>
-            <h3 class="login">LOGIN</h3>
-            <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a>
-*/
-const navBarMenu = document.getElementById("navbar")
+const liOptions = document.querySelectorAll("li");
 
-if(innerWidth <= 764){
-
-    const nytImg = document.createElement("img")
-    nytImg.setAttribute("id","imgNav")
-    nytImg.setAttribute("src","../Assets/NYT.png")
-    navBarMenu.append(nytImg)
-
-    const bocataDesplegable = document.createElement("div")
-    bocataDesplegable.setAttribute("id","bocata")
-
-    const slice1 = document.createElement("div")
-    slice1.setAttribute("id","slice1")
-    const slice2 = document.createElement("div")
-    slice2.setAttribute("id","slice2")
-    const slice3 = document.createElement("div")
-    slice3.setAttribute("id","slice3")
-
-    bocataDesplegable.append(slice1)
-    bocataDesplegable.append(slice2)
-    bocataDesplegable.append(slice3)
-
-
-    navBarMenu.append(bocataDesplegable)
-
-    const divOptions = document.createElement("div")
-    divOptions.setAttribute("id","options")
-
-    const h3Dash = document.createElement("h3")
-    h3Dash.setAttribute("class","dash")
-    h3Dash.innerHTML = "DASHBOARD"
-    const h3Details = document.createElement("h3")
-    h3Details.setAttribute("class","detail")
-    h3Details.innerHTML = "DETAILS"
-    const h3Login = document.createElement("h3")
-    h3Login.setAttribute("class","login")
-    h3Login.innerHTML = "LOGIN"
-   
-    h3Dash.hidden = true
-    h3Details.hidden = true
-    h3Login.hidden = true
-    
-    divOptions.append(h3Dash)
-    divOptions.append(h3Details)
-    divOptions.append(h3Login)
-
-    
-    
-
-    bocataDesplegable.onclick = () => {
-        if(h3Dash.hidden === true){
-            h3Dash.hidden = false
-            h3Details.hidden = false
-            h3Login.hidden = false
-            bocataDesplegable.append(divOptions);
-            slice1.remove()
-            slice2.remove()
-            slice3.remove()
-            
-        }else{
-            h3Dash.hidden = true
-            h3Details.hidden = true
-            h3Login.hidden = true
-            divOptions.remove()
-            bocataDesplegable.append(slice1)
-            bocataDesplegable.append(slice2)
-            bocataDesplegable.append(slice3)
-        }
-    }
-
-
-
-
-
-    
-
-
+liOptions[0].onclick = () => {
+    location.replace("../index.html")
 }
